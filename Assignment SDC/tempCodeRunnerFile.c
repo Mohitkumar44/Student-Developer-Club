15adbba5af3a0f932872774a9be2647510aeb0e3
Mohit Kumar <mohit.kumar162534@gmail.com>
2025-06-15 22:07:22 +0530

    Update

--- Assignment SDC/tempCodeRunnerFile.c
+++ Assignment SDC/tempCodeRunnerFile.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
-
-int Sum_of_even(int arr[],int len);
-int Sum_of_even(int arr[],int len){
-    int sum = 0;
-    for (int i = 0; i < len; i++)
-    {
-        if (arr[i]%2==0)
-        {
-            sum = sum + arr[i];
+void printFloydTriangle(int n) {
+    // Write your code here
+    int k=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("%d ",k);
+            k++;
         }
+        printf("\n");
     }
-    return sum;
 }
+
 int main() {
-    int arr[] = {9,8465,984,65,498,46,549,874,654,984,594,84,64,31,49,876,5413,764,34,68,4513,873541,3,4,374,354,9,78468,796,3};
-    int len = sizeof(arr)/sizeof(arr[0]);
-    int sum = Sum_of_even(arr,len);
-    printf("sum of all even elements in given array is : %d",sum);
+    printFloydTriangle(4);
     return 0;
 }
+
